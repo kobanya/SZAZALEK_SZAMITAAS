c88362800dc8516a36df4389dd05837b848cc75d
kobanya <nagy.bela.budapest@gmail.com>
2023-03-20 22:35:50 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+'''
+
+SZázalékszámítás
+
+'''
+
+print("Százalékszámítás")
+
+alap = int(input("Add meg az alapot: "))
+ertek = int(input('Add meg a százalék értéket: '))
+print( "alap:%d, érték: %d, százalékéláb: %.2f" % (alap, ertek, ertek/alap*100))
